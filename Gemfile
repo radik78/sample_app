f0e18f7873e3ee4bc1d8984b9b367ba79414e4e3
radik <iq30@mail.ru>
2011-11-17 20:59:25 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+gem 'jquery-rails'
+
+group :development do
+  gem 'rspec-rails', '2.6.1'
+  gem 'sqlite3', '1.3.4'
+end
+
+group :test do
+  gem 'rspec-rails', '2.6.1'
+  gem 'webrat', '0.7.1'
+end
+
+group :production do
+    gem "pg"	# gems specifically for Heroku go here
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '8c1291cdc0a37a2f787b70b86eb85bc5865e77d8c6f3f403a4733a9356aea2b79911fc9c8d1c7637471aeb0ef35c4321640e2cd1dbac0642fe68f310cbb93085'
